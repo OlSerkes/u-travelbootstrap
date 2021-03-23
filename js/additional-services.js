ca59142b2813a3666c0fdedfd8a5d8bf4a74cea9
OlSerkes <serkes@i.ua>
2021-03-23 23:15:11 +0200

    Update additional-services.js
    Changed js

--- js/additional-services.js
+++ js/additional-services.js
@@ -1,63 +1,62 @@
-const formadditional = document.getElementByld('formadditional');
-const name = document.getElementByld('name');
-const phonenumber = document.getElementByld('phone-number');
-const email = document.getElementByld('email');
+const formadditional = document.getElementById('formadditional');
+const name = document.getElementById('name');
+const phonenumber = document.getElementById('phone-number');
+const email = document.getElementById('email');
 
-//Show input error message
+// Show input error message
 function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
 }
-//Show succes outline
+
+// Show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
-//Check email is valid
+// Check email is valid
 function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
-        showSuccess(input);
-    } else {
-        showError(input, 'Email is not valid');
-    }
-    }
-//Check requared fields
-function checkRequired(inputArr) {
-    let isRequired = false;
-    inputArr.forEach(function(input) {
-        if (input.value.trim() ==="") {
-            showError(input, `${getFieldName(input)} is rrequired`);
-            isRequired = true;
-            } else {
-                showSuccess(input);
-            }
-    });
-
-    return isRequired;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
-//Check input length
-function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(
-            input,
-            `${getFieldName(input)} must be at least ${min} characters`
-        );
-    } else if (input.value.length > max) {
-        showError(
-            input,
-            `${getFieldName(input)} must be less than ${max} characters`
-            );
-         } else {
-            showSucces(input); 
-         }
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
     }
-    //Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  });
+
+  return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
 }
 
 // Event listeners

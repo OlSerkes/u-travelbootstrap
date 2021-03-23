745817bf2785e3cc24c385d138e700dc07021840
OlSerkes <serkes@i.ua>
2021-03-23 20:27:59 +0000

    Added js file

---
+++ js/additional-services.js
@@ -0,0 +1,59 @@
+const formadditional = document.getElementById('formadditional');
+const name = document.getElementById('name');
+const phonenumber = document.getElementById('phone-number');
+const email = document.getElementByld('email');
+
+//Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//Show succes outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+    }
+//Check requared fields
+function checkRequired(inputArr) {
+    let isRequired = false;
+    inputArr.forEach(function(input) {
+        if (input.value.trim() ==="") {
+            showError(input, `${getFieldName(input)} is rrequired`);
+            isRequired = true;
+            } else {
+                showSuccess(input);
+            }
+    });
+
+    return isRequired;
+}
+//Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+            );
+         } else {
+            showSucces(input); 
+         }
+    }
+    //
+

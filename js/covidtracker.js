5bbc808c2ac683c9dcb4452e7ea600050a71af7a
OlSerkes <serkes@i.ua>
2021-03-30 18:29:54 +0000

    Changed covi tracker

--- js/covidtracker.js
+++ js/covidtracker.js
@@ -1,2 +1,17 @@
 
+document.querySelector('.')
+$get.JSON("https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions/UKR/2021-03-30",
+ function(data){
+     console.log(data);
+
+     const workschool = data.policy_type_display;
+     $('.work-school').append(workschool);
+
+     const weather = data.weather.main;
+     $('.weather').append(weather);
+
+     const temp = Math.floor(data.main.temp);
+     $('.temp').append(temp);
+     
+ })
    

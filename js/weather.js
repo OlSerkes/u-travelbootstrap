6bc208743fa94541f23c4319ba5c2980dbfbded0
OlSerkes <serkes@i.ua>
2021-03-30 18:05:18 +0000

    added weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,17 @@
-$get.JSON("http://api.openweathermap.org/data/2.5/weather?q=Kyiv,ua&units=metric&APPID=e0ac0b668139f06627dd644c22f3ac6d",
+const city = "Kyiv"
+
+
+$get.JSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=e0ac0b668139f06627dd644c22f3ac6d",
  function(data){
      console.log(data);
 
      const icon = "http://openweathermap.org/img/w/" + data.weather.icon + ".png";
-     $(".icon").attr("src", icon);
-     
-     
+     $('.icon').attr("src", icon);
 
+     const weather = data.weather.main;
+     $('.weather').append(weather);
+
+     const temp = Math.floor(data.main.temp);
+     $('.temp').append(temp);
+     
  })
